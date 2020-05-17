b9d6b1fd0de72a805e5f0bcecafdecbcaf2aa698
Дмитро <ortho@MacBook-Air.local>
2020-05-17 15:30:10 +0300

    reccord

---
+++ js/index.js
@@ -0,0 +1,10 @@
+alert('Шановні пацієнти! В період карантину ми працюємо та дотримуємось усіх протиепідемічних заходів щодо розповсюдження та зараження COVID 19.')
+
+  
+const clock = document.querySelector('.clock');
+const options = {
+    year: "numeric", month: "numeric",
+    day: "numeric", hour: "2-digit", minute: "2-digit", second: "2-digit"
+};
+const showClock = () => clock.innerText = (new Date()).toLocaleTimeString("uk", options);
+setInterval(showClock, 1000);
